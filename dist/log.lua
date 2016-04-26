d3ddb2f316c063eb2e345b4c33a3095d8dcca30d
f4rnham <f4rnham@users.noreply.github.com>
2016-04-27 00:47:50 +0200

    Logging: Drop support for custom log callback (not needed anymore) and suppress "changing loglevel..." message

--- dist/log.lua
+++ dist/log.lua
@@ -4,25 +4,25 @@ require "logging.console"
 
 local logger_console = nil
 local logger_file = nil
-local logger_hook = nil
 
 function reload_config(cfg, hook)
-    logger_console = logging.console("%level %message\n")
-    logger_file = logging.file(cfg.log_file_abs)
-
     if cfg.print_log_level then
+        logger_console = logging.console("%level %message\n")
+        -- Hack to prevent "changling loglevel..." message
+        logger_console.level = nil
         logger_console:setLevel(cfg.print_log_level)
     else
         logger_console = nil
     end
 
     if cfg.write_log_level then
+        logger_file = logging.file(cfg.log_file_abs)
+        -- Hack to prevent "changling loglevel..." message
+        logger_file.level = nil
         logger_file:setLevel(cfg.write_log_level)
     else
         logger_file = nil
     end
-
-    logger_hook = hook
 end
 
 local logger = logging.new(
@@ -33,10 +33,6 @@ function(self, level, message)
 
     if logger_file then
         logger_file:log(level, message)
-    end
-
-    if logger_hook then
-        logger_hook(level, message)
     end
 
     return true
